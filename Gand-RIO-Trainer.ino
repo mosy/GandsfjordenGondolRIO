44200a374c4c90b2fad9d77bb3c08ec8a74c74f0
Fred-Olav <fred-olav@mosdal.org>
2023-03-15 16:01:24 +0100

    14DI og 2 AI virker

--- Gand-RIO-Trainer.ino
+++ Gand-RIO-Trainer.ino
@@ -21,22 +21,20 @@
 
 //Modbus Registers Offsets
 //Used Pins
-const int DI1 = 4; //GPIO0
-const int DI2 = 16; //GPIO0
-const int DI3 = 17; //GPIO0
-const int DI4 = 18; //GPIO0
-const int DI5 = 19; //GPIO0
-const int DI6 = 21; //GPIO0
-const int DI7 = 22; //GPIO0
-const int DI8 = 23; //GPIO0
-const int DO1 = 2; //GPIO0
-const int DO2 = 33; //GPIO0
-const int DO3 = 32; //GPIO0
-const int DO4 = 25; //GPIO0
-const int DO5 = 26; //GPIO0
-const int DO6 = 27; //GPIO0
-const int DO7 = 14; //GPIO0
-const int DO8 = 12; //GPIO0
+const int DI1 = 34; //GPIO0
+const int DI2 = 35; //GPIO0
+const int DI3 = 32; //GPIO0
+const int DI4 = 33; //GPIO0
+const int DI5 = 27; //GPIO0
+const int DI6 = 14; //GPIO0
+const int DI7 = 12; //GPIO0
+const int DI8 = 13; //GPIO0
+const int DI9 = 15; //GPIO0
+const int DI10 = 2; //GPIO0
+const int DI11 = 4; //GPIO0
+const int DI12 = 5; //GPIO0
+const int DI13 = 18; //GPIO0
+const int DI14 = 19; //GPIO0
 
 // Modbus Registers Offsets
 const int TEST_HREG = 1;
@@ -64,40 +62,51 @@ void setup() {
   mb.server();
   mb.addHreg(TEST_HREG, 0x000A);
       //Set ledPin mode
-    pinMode(DI1, INPUT_PULLDOWN);
-    pinMode(DI2, INPUT_PULLDOWN);
-    pinMode(DI3, INPUT_PULLDOWN);
-    pinMode(DI4, INPUT_PULLDOWN);
-    pinMode(DI5, INPUT_PULLDOWN);
-    pinMode(DI6, INPUT_PULLDOWN);
-    pinMode(DI7, INPUT_PULLDOWN);
-    pinMode(DI8, INPUT_PULLDOWN);
-    pinMode(DO1, OUTPUT);
-    pinMode(DO2, OUTPUT);
-    pinMode(DO3, OUTPUT);
-    pinMode(DO4, OUTPUT);
-    pinMode(DO5, OUTPUT);
-    pinMode(DO6, OUTPUT);
-    pinMode(DO7, OUTPUT);
-    pinMode(DO8, OUTPUT);
+    pinMode(DI1, INPUT);
+    pinMode(DI2, INPUT);
+    pinMode(DI3, INPUT_PULLUP);
+    pinMode(DI4, INPUT_PULLUP);
+    pinMode(DI5, INPUT_PULLUP);
+    pinMode(DI6, INPUT_PULLUP);
+    pinMode(DI7, INPUT_PULLUP);
+    pinMode(DI8, INPUT_PULLUP);
+    pinMode(DI9, INPUT_PULLUP);
+    pinMode(DI10, INPUT_PULLUP);
+    pinMode(DI11, INPUT_PULLUP);
+    pinMode(DI12, INPUT_PULLUP);
+    pinMode(DI13, INPUT_PULLUP);
+    pinMode(DI14, INPUT_PULLUP);
     // Add SWITCH_ISTS register - Use addIsts() for digital inputs
-	mb.addIsts(1,false,8);
-	mb.addCoil(1,false,8);
+	mb.addIsts(1,false,14);
+	mb.addHreg(1, 0,2);
+//	mb.addCoil(1,false,8);
 }
  
 void loop() {
    //Call once inside loop() - all magic here
    mb.task();
     //Attach DI1 to SWITCH_ISTS register
-	mb.Ists(1, digitalRead(DI1));
-	mb.Ists(2, digitalRead(DI2));
-	mb.Ists(3, digitalRead(DI3));
-	mb.Ists(4, digitalRead(DI4));
-	mb.Ists(5, digitalRead(DI5));
-	mb.Ists(6, digitalRead(DI6));
-	mb.Ists(7, digitalRead(DI7));
-	mb.Ists(8, digitalRead(DI8));
+	mb.Ists(1, !digitalRead(DI1));
+	mb.Ists(2, !digitalRead(DI2));
+	mb.Ists(3, !digitalRead(DI3));
+	mb.Ists(4, !digitalRead(DI4));
+	mb.Ists(5, !digitalRead(DI5));
+	mb.Ists(6, !digitalRead(DI6));
+	mb.Ists(7, !digitalRead(DI7));
+	mb.Ists(8, !digitalRead(DI8));
+	mb.Ists(9, !digitalRead(DI9));
+	mb.Ists(10, !digitalRead(DI10));
+	mb.Ists(11, !digitalRead(DI11));
+	mb.Ists(12, !digitalRead(DI12));
+	mb.Ists(13, !digitalRead(DI13));
+	mb.Ists(14, !digitalRead(DI14));
 
+
+	mb.Hreg(1, analogRead(36));
+	mb.Hreg(2, analogRead(39));
+	
+/*
+        potValue = analogRead(potPin);
 	digitalWrite(DO1, mb.Coil(1));
 	digitalWrite(DO2, mb.Coil(2));
 	digitalWrite(DO3, mb.Coil(3));
@@ -106,6 +115,7 @@ void loop() {
 	digitalWrite(DO6, mb.Coil(6));
 	digitalWrite(DO7, mb.Coil(7));
 	digitalWrite(DO8, mb.Coil(8));
+	*/
    delay(10);
 
 }
